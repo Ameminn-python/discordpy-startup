c3fe9b736e5fe0e123c44330e9bfd99d05d63ec0
Ameminn-python <63637891+Ameminn-python@users.noreply.github.com>
2020-04-14 19:12:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,18 @@
-from discord.ext import commands
+import discord
 import os
 import traceback
-
-bot = commands.Bot(command_prefix='/')
+client=discord.client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
+@client.event
+async def on_message(message):
+    if message.content == '/ch':
+        await message.channel.send(str(message.channel.name)+':'+str(message.channel.id))
+    if message.content == '/guild':
+        await message.channel.send(str(message.guild.name)+':'+str(message.guild.id))
+    if message.content.startswith('/role'):
+        for role in message.role_mentions:
+            await message.channel.send(str(role.name)+';'+str(role.id))
+    
 bot.run(token)
